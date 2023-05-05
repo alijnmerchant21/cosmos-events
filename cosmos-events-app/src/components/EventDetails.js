2d7d458df3c0c40bca53d4c57ed8930cb3160559
alijnmerchant21 <amercha1@stevens.edu>
2023-05-05 15:57:23 +0100

    Update EventDetails to show list of attendees only to logged in users

--- cosmos-events-app/src/components/EventDetails.js
+++ cosmos-events-app/src/components/EventDetails.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { auth } from '../firebase';
 
 function EventDetails() {
+  const [attendees, setAttendees] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        // Fetch attendees from the database and set the state
+      } else {
+        setAttendees([]);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div>
       <h1>Event Details</h1>
       <p>Details of the event here</p>
+      {attendees.length > 0 && (
+        <div>
+          <h2>Attendees</h2>
+          <ul>
+            {attendees.map((attendee) => (
+              <li key={attendee.id}>{attendee.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
