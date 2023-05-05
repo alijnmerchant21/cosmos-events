6f7410dc463a5aa2bd6e6a57d1d591eaa568e999
alijnmerchant21 <amercha1@stevens.edu>
2023-05-05 15:47:57 +0100

    Create EventDetails.js

---
+++ cosmos-events-app/src/components/EventDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EventDetails() {
+  return (
+    <div>
+      <h1>Event Details</h1>
+      <p>Details of the event here</p>
+    </div>
+  );
+}
+
+export default EventDetails;
