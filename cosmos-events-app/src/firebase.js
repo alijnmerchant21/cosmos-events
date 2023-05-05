a27157fae1b15de5c042f862533dd7073fe04928
alijnmerchant21 <amercha1@stevens.edu>
2023-05-05 16:06:50 +0100

    Add config details to firebase app

--- cosmos-events-app/src/firebase.js
+++ cosmos-events-app/src/firebase.js
@@ -2,7 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  // Your Firebase configuration here
+  apiKey: "AIzaSyBpOp_wbhzoABUbQPZBF4rwF244l6oA3xw",
+  authDomain: "cosmos-events-33c78.firebaseapp.com",
+  projectId: "cosmos-events-33c78",
+  storageBucket: "cosmos-events-33c78.appspot.com",
+  messagingSenderId: "758933846252",
+  appId: "1:758933846252:web:92e1101dcb919ae25b0dc9",
+  measurementId: "G-R3TZ39L18J"
 };
 
 firebase.initializeApp(firebaseConfig);
