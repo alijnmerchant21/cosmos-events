4a16e99cd4d6a11fbe70ae748475b96baa5c5874
alijnmerchant21 <amercha1@stevens.edu>
2023-05-05 16:45:14 +0100

    Create SubmitEventForm
    Manage form input

---
+++ cosmos-events-app/src/components/SubmitEventForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { db } from '../firebase';
+
+function SubmitEventForm() {
+  const [eventName, setEventName] = useState('');
+  const [eventLocation, setEventLocation] = useState('');
+  const [eventTime, setEventTime] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    db.collection('events').add({
+      name: eventName,
+      location: eventLocation,
+      time: eventTime,
+    })
+    .then(() => {
+      history.push('/');
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+  };
+
+  return (
+    <div>
+      <h2>Submit an Event</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Event Name:
+          <input type="text" value={eventName} onChange={(e) => setEventName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Event Location:
+          <input type="text" value={eventLocation} onChange={(e) => setEventLocation(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Event Time:
+          <input type="text" value={eventTime} onChange={(e) => setEventTime(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SubmitEventForm;
